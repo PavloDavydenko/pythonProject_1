f59d638f342d6d37fdb63b1a135f760b972715bd
Pavlo <105214761+PavloDavydenko@users.noreply.github.com>
2024-04-27 15:53:38 +0300

    Fourth commit

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 if __name__ == '__main__':
     print('Hello')
     print(add(3, 4))
     print(sub(3, 4))
+    print(mul(2, 3))
